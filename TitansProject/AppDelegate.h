015e4e1b14242b409c9d8f5b6832bbee2090c3ad
geasscode <geasscode@outlook.com>
2014-06-20 11:47:05 +0800

    Initial Commit

---
+++ TitansProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TitansProject
+//
+//  Created by desmond on 6/20/14.
+//  Copyright (c) 2014 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TitansProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TitansProject
+//
+//  Created by desmond on 6/20/14.
+//  Copyright (c) 2014 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
