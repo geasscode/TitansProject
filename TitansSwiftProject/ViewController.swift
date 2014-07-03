7230f2fb457da15d589b608d7d57756cbc1b27a3
geasscode <geasscode@outlook.com>
2014-07-03 18:50:36 +0800

    add line function

--- TitansSwiftProject/ViewController.swift
+++ TitansSwiftProject/ViewController.swift
@@ -71,13 +71,4 @@ class ViewController: UIViewController {
     
 }
 
-override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-}
-
-
-
-
-}
 
