a635a8264fdf4daed66ce0cbac7245d496e87930
geasscode <geasscode@outlook.com>
2014-07-04 19:01:32 +0800

    add json swift , and swift function.

--- TitansSwiftProject/ViewController.swift
+++ TitansSwiftProject/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        testOddEven()
+        loadJSONFile()
         //new add
         // Example #1
         //
@@ -69,6 +70,78 @@ class ViewController: UIViewController {
         return true
     }
     
+    func loadJSONFile() -> Void{
+        var validJSONData:NSData!
+
+        validJSONData = NSData(contentsOfFile : NSBundle(forClass: ViewController.self).pathForResource("Valid", ofType: "JSON"))
+        let json = JSONValue(validJSONData)
+        
+        let stringValue = json["title"].string
+        let numberValue = json["id"].number
+        let boolValue = json["user"]["site_admin"].bool
+        let nullValue = json["closed_by"]
+        let arrayValue = json["labels"].array
+        let objectValue = json["user"].object
+
+        println("the resul is \(stringValue),\(boolValue)")
+        
+        switch json["user"]{
+        case .JString(let stringValue):
+            let id = stringValue.toInt()
+        case .JNumber(let numberValue):
+            let id = numberValue.integerValue
+        case .JObject(let ObjectValue):
+            let id = objectValue
+        default:
+            println("ooops!!! JSON Data is Unexpected or Broken")
+
+        
+        var customerData:NSData!
+        
+        //Case sensitive
+        customerData = NSData(contentsOfFile : NSBundle(forClass: ViewController.self).pathForResource("customers", ofType: "JSON"))
+        let customer = JSONValue(customerData)
+        let url = customer["url"].string
+        let orders = customer["customers"].array
+        let testOrder = customer["customers"][2]["orders"]["order"][0]["total"].string
+
+        println("tootal resul is \(url),testOrder is \(testOrder)")
+
+        let handleErrorMessage = JSONValue(customerData)["customers"].array
+        
+      
+        
+        if handleErrorMessage{
+            println("tootal resul is \(url),order is \(orders)")
+
+        }
+//        else{
+//            println(handleErrorMessage)
+//         
+//            switch handleErrorMessage{
+//                
+//            case .JInvalid(let error):
+//                println()
+//                
+//            default :
+//                println()
+//            }
+//        }
+    
+    }
+    
 }
+    
+    func testOddEven()
+    {
+        let numbers = (100...150)
+            .map { n -> (Number:Int, OddEven:String) in
+                (n, n % 2 == 1 ? "odd" : "even")
+        }
+        
+        for n in numbers {
+            println("The number \(n.Number) is \(n.OddEven).")
+        }
+    }
 
-
+}
